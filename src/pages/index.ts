5c338117f54b33ed8fc1cf68a94c1ef171a57669
mohamed.tahiri <mhdtahiri01@gmail.com>
2026-01-04 15:46:20 +0100

    feat: add pages barrel export

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as OverviewPage } from './OverviewPage';
+export { default as MonitoringPage } from './MonitoringPage';
+export { default as AnalyticsPage } from './AnalyticsPage';
+export { default as SecurityPage } from './SecurityPage';
+export { default as SettingsPage } from './SettingsPage';
